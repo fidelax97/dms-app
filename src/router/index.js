2c0e0157773ea19694dbf7584494eea50725e671
Fidel Anak Dublin <fidelad@sains.com.my>
2022-11-08 11:23:55 +0800

    Update config

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/',
+      path: '/login',
       name: 'Login',
       component: () => import("../views/login"),
     },
